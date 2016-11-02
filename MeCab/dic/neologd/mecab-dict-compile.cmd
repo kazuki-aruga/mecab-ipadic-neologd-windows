53e93f840ed00ff829ab908e928325f40f8f751e
ARUGA, Kazuki <kazuki.aruga@gmail.com>
2016-11-02 13:33:24 +0900

    neologdの*.dicファイルをVirtualStoreではなくProgram Filesに明示的に配置するように修正

--- MeCab/dic/neologd/mecab-dict-compile.cmd
+++ MeCab/dic/neologd/mecab-dict-compile.cmd
@@ -2,4 +2,4 @@
 
 set MECAB_HOME="C:\Program Files (x86)\MeCab"
 
-%MECAB_HOME%\bin\mecab-dict-index.exe -d %MECAB_HOME%\dic\ipadic -u %2 -f shift-jis -t shift-jis %1
+%MECAB_HOME%\bin\mecab-dict-index.exe -d %MECAB_HOME%\dic\ipadic -u %2 -f shift-jis -t shift-jis %MECAB_HOME%\dic\neologd\%1
